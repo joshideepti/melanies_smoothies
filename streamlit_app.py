1a5fc41bed144bdde7baf9e8761fb19206adbfa2
joshideepti <144677418+joshideepti@users.noreply.github.com>
2024-07-21 03:28:28 +0530

    Update streamlit_app.py
    try catch added

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,21 +45,23 @@ if ingredients_list:
         ingredients_string += fruit_chosen + ' '
                 
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        try:
         if search_on:
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-            st.subheader(fruit_chosen + '  Nutrition Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-            fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
-            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-        elif fruit_chosen:
-            st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-            st.subheader(fruit_chosen + '  Nutrition Information')
-            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-            fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
-            my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
-                            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-            
+                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                st.subheader(fruit_chosen + '  Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+                fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
+                my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+            elif fruit_chosen:
+                st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+                st.subheader(fruit_chosen + '  Nutrition Information')
+                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
+                fv_df = st.dataframe(data = fruityvice_response.json(), use_container_width=True)
+                my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+                                values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+            except:
+                 st.write('Oops! Something went wrong')
     time_to_insert =st.button('Submit Order')
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
